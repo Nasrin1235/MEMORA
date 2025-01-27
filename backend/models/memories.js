aeda238b358b376575da850e62f7457df5fd1b25
Nasrin1235 <nasrin.mahmoudi@gmx.de>
2025-01-27 14:37:15 +0100

    update memories

---
+++ backend/models/memories.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+
+const memoriesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    memory: {
+      type: String,
+      required: true,
+    },
+    visitedLocation: {
+      type: [String], 
+      default: [], 
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User', 
+      required: true,
+    },
+    createdOn: {
+      type: Date,
+      default: Date.now,
+    },
+    imageUrl: {
+      type: String,
+      required: false, 
+    },
+    visitedDate: {
+      type: Date,
+      required: false,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+
+export const Memories = mongoose.model("Memories", memoriesSchema);
+
+
+
+
+
+
+
+
+
