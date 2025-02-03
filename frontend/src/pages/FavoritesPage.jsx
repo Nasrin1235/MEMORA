09236597131b625017afb5621ed06c67f68b07f4
Nasrin1235 <nasrin.mahmoudi@gmx.de>
2025-02-03 15:49:14 +0100

    favorites

--- frontend/src/pages/FavoritesPage.jsx
+++ frontend/src/pages/FavoritesPage.jsx
@@ -1,12 +1,30 @@
+import { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
-
+import MemoryList from "../components/MemoryList.jsx";
 
 const FavoritesPage = () => {
+  const [favorites, setFavorites] = useState([]);
+  useEffect(() => {
+    const savedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    console.log("Loaded favorites on page:", savedFavorites); 
+    setFavorites(savedFavorites);
+  }, []);
   return (
     <div>
-    <Sidebar setFilteredMemories={() => {}} />
-  </div>
+      <Sidebar setFilteredMemories={() => {}} />
+      <div className="memory-list-container">
+        <h2>:stern: Favorites</h2>
+        {favorites.length === 0 ? (
+          <p>No favorite memories yet.</p>
+        ) : (
+          <MemoryList
+            memories={favorites}
+            filteredMemories={null}
+            onMemorySelect={() => {}}
+          />
+        )}
+      </div>
+    </div>
   );
 };
-
 export default FavoritesPage;
