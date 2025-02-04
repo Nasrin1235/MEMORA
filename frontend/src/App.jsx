994340e8bc5c1edc6c528628694dabda2ff4a778
OlgaTymofieieva <olha.tymofieieva@dci-student.org>
2025-02-04 10:47:35 +0100

    context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
-import {  Routes, Route } from "react-router-dom"
-import LoginPage from "./pages/LoginPage"
-import RegisterPage from "./pages/RegisterPage"
-import MainPage from "./pages/MainPage"
-import HomePage from "./pages/Homepage"
-import CalendarPage from "./pages/CalendarPage"
-import AtlasPage from "./pages/AtlasPage"
-import FavoritesPage from "./pages/FavoritesPage"
-
+import {  Routes, Route } from "react-router-dom";
+import { MemoryProvider } from "./context/MemoryContext";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import MainPage from "./pages/MainPage";
+import HomePage from "./pages/Homepage";
+import CalendarPage from "./pages/CalendarPage";
+import AtlasPage from "./pages/AtlasPage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
   return (
-
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/main" element={<MainPage/>} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/atlas" element={<AtlasPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        
-      </Routes>
-  )
+    <MemoryProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/atlas" element={<AtlasPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+    </MemoryProvider>
+  );
 }
 
-export default App
+export default App;
