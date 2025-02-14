eb71faaffcd454adc3736d009e3ad03283bbbd3b
OlgaTymofieieva <olha.tymofieieva@dci-student.org>
2025-02-14 13:52:33 +0100

    l

--- frontend/src/components/MobileHeader.jsx
+++ frontend/src/components/MobileHeader.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import { Search, Plus } from "lucide-react";
 import AddMemoryForm from "./AddMemoryForm";
+import { useLocation } from "react-router-dom";
 import "../styles/MobileHeader.css";
 
 const MobileHeader = ({ onSearch }) => {
   const [searchVisible, setSearchVisible] = useState(false);
   const [showForm, setShowForm] = useState(false);
   const [searchText, setSearchText] = useState("");
+
+  const location = useLocation();
+  const hiddenHeaderPaths = ["/login", "/register", "/"];
+
+  if (hiddenHeaderPaths.includes(location.pathname)) {
+    return null;
+  }
 
   return (
     <div className="mobile-header">
@@ -18,17 +26,17 @@ const MobileHeader = ({ onSearch }) => {
       </button>
 
       {searchVisible && (
-       <input
-       type="text"
-       className={`mobile-search-input ${searchVisible ? "visible" : ""}`} 
-       placeholder="Search memories..."
-       value={searchText}
-       onChange={(e) => {
-         setSearchText(e.target.value);
-         onSearch(e.target.value); 
-       }}
-     />
-   )}
+        <input
+          type="text"
+          className={`mobile-search-input ${searchVisible ? "visible" : ""}`}
+          placeholder="Search memories..."
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+            onSearch(e.target.value);
+          }}
+        />
+      )}
       <button
         className="mobile-add-story-btn"
         onClick={() => setShowForm(true)}
